7ab7c626ce3e59447c46569131658a3754de4af1
Yasser Santiago <yasser.santiagoe@gmail.com>
2023-04-07 22:41:48 +0200

    Frist Commit

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,14 @@
+<template>
+    <img
+      :src="tweet.user.image"
+      class="profile"
+      alt="profile"
+    />
+</template>
+
+<script setup>
+defineProps {(
+    
+)}
+
+</script>
--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,24 +1,19 @@
 <template>
   <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
+    
 
     <div class="body">
       <div class="top">
         <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
+          <span class="name" >{{ tweet.user.name }}</span>
+          <span class="handle">{{ tweet.user.handle }}</span>
         </span>
 
-        <span class="timestamp">Nov 30, 2020</span>
+        <span class="timestamp">{{ tweet.timestamp }}</span>
       </div>
 
       <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
+        {{ tweet.message }}
       </p>
 
       <div class="actions">
@@ -33,6 +28,15 @@
     <i class="fas fa-ellipsis-h"></i>
   </div>
 </template>
+
+<script setup>
+
+
+defineProps({
+  tweet: Object,
+})
+
+</script>
 
 <style scoped>
 a {
